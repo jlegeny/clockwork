fd2eeeef8a434a0ab38817e20c80b3da46b44147
yoz-y <yoz-y@pexis.otaku.sk>
2014-11-12 01:43:41 +0100

    Do not rsync data if middleman build fails

--- deploy.pl
+++ deploy.pl
@@ -2,7 +2,7 @@
 
 die "Environment not set" unless $ENV{CLOCKWORKSSH};
 
-print `middleman build`;
-print `rsync -avz -e ssh build/ $ENV{CLOCKWORKSSH}:clockwork/`;
+system("middleman build");
 
-	
+system("rsync -avz -e ssh build/ $ENV{CLOCKWORKSSH}:clockwork/") unless $?;
+
